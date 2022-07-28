8067d8385d83d1649f3006c502747c2e10eacf44
mezz <tehgeek@gmail.com>
2022-07-27 19:14:53 -0700

    Update for Minecraft 1.19

--- src/main/java/mezz/itemzoom/client/KeyBindings.java
+++ src/main/java/mezz/itemzoom/client/KeyBindings.java
@@ -2,18 +2,25 @@ package mezz.itemzoom.client;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
+	@Nullable
+	private static KeyBindings INSTANCE;
 	public final KeyMapping toggle;
 	public final KeyMapping hold;
 	public final KeyMapping zoomIn;
 	public final KeyMapping zoomOut;
 
-	public KeyBindings() {
+	public static void create(RegisterKeyMappingsEvent registerEvent) {
+		INSTANCE = new KeyBindings(registerEvent);
+	}
+
+	private KeyBindings(RegisterKeyMappingsEvent registerEvent) {
 		InputConstants.Key zKey = InputConstants.Type.KEYSYM.getOrCreate(GLFW.GLFW_KEY_Z);
 		InputConstants.Key none = InputConstants.UNKNOWN;
 		String category = Constants.MOD_NAME;
@@ -24,7 +31,14 @@ public class KeyBindings {
 			zoomOut = new KeyMapping("key.itemzoom.zoom.out", KeyConflictContext.GUI, KeyModifier.NONE, none, category)
 		};
 		for (KeyMapping binding : allBindings) {
-			ClientRegistry.registerKeyBinding(binding);
+			registerEvent.register(binding);
 		}
+	}
+
+	public static KeyBindings getInstance() {
+		if (INSTANCE == null) {
+			throw new IllegalStateException("Keybindings have not been created");
+		}
+		return INSTANCE;
 	}
 }
